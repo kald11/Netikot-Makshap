319b163dcae53974d6a4efa93eddcfe79cab3158
Itai <itaitsayeg@gmail.com>
2024-12-22 11:43:19 +0200

    open new file

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from core.google_sheets import GoogleSheets
 def main():
     df = GoogleSheets().get_data()
     print(df.values)
-    print("maor")
-    print("change") 
 
 
 if __name__ == "__main__":
