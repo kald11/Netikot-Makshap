a9c95c3bec859689eba54db2353d3a37dae1388d
Maor Zohar <maorzohar14@gmail.com>
2025-01-05 15:40:33 +0200

    Finish do pings to cameras and nvr + prints

--- core/google_sheets.py
+++ core/google_sheets.py
@@ -1,7 +1,8 @@
+import time
+
 from config.settings import Config
 import gspread
 from utils.utils import columns_to_rows_array, array_to_df, convert_to_sites_array
-from pathlib import Path
 
 
 class GoogleSheets:
@@ -11,20 +12,28 @@ class GoogleSheets:
         self.connection = gspread.service_account(
             filename=self.config["google_sheets"]["service_account_path"])
         spread_sheet = self.connection.open(self.config["google_sheets"]["spreadsheet_name"])
-        self.worksheet = spread_sheet.worksheet(self.config["google_sheets"]["worksheet_name"])
+        self.input_worksheet = spread_sheet.worksheet(self.config["google_sheets"]["input_worksheet_name"])
+        self.output_worksheet = spread_sheet.worksheet(self.config["google_sheets"]["output_worksheet_name"])
 
     def get_data(self):
+        print("-------------- Fetch Data is starting -------------------")
+        start_time = time.perf_counter()
         columns_indexes = self.convert_column_names_to_indexes(self.config["google_sheets"]["desired_column_names"])
-        columns = [self.worksheet.col_values(col) for col in columns_indexes]
+        columns = [self.input_worksheet.col_values(col) for col in columns_indexes]
         rows = columns_to_rows_array(columns)
         df = array_to_df(rows)
-        return convert_to_sites_array(df)
+        sites = convert_to_sites_array(df)
+        end_time = time.perf_counter()
+        execution_time = end_time - start_time
+        print(f"----------------------- Fetch Data ends in {execution_time:.6f} seconds ------------------------------")
+        return sites
 
     def convert_column_names_to_indexes(self, column_names):
-        headers = self.worksheet.row_values(1)
-        return [headers.index(name)+1 for name in column_names if name in headers]
+        headers = self.input_worksheet.row_values(1)
+        return [headers.index(name) + 1 for name in column_names if name in headers]
 
     def get_columns_values(self, columns_indexes):
-        return [self.worksheet.col_values(col)[1:] for col in columns_indexes]
+        return [self.input_worksheet.col_values(col)[1:] for col in columns_indexes]
 
-
+    def upload_data(self, data):
+        self.output_worksheet.insert_rows(data, row=2)
--- core/services.py
+++ core/services.py
@@ -1,4 +1,6 @@
-from utils.utils import use_thread
+import time
+
+from utils.utils import use_thread, get_results_array
 
 
 class NetikotService:
@@ -7,11 +9,19 @@ class NetikotService:
         self.cameras_array = cameras_array
 
     def ping(self):
+        print("-------------- Ping is starting -------------------")
+        start = time.perf_counter()
+
         def worker(camera):
             camera.ping_camera()
             camera.ping_nvr()
-
         use_thread(self.cameras_array, worker)
+
+        results = get_results_array(self.cameras_array)
+        end = time.perf_counter()
+        execution_time = end - start
+        print(f"----------------------- Fetch Data ends in {execution_time:.6f} seconds ------------------------------")
+        return results
 
     def get_camera_data(self):
         def worker(camera):
@@ -19,4 +29,3 @@ class NetikotService:
             camera.get_current_time()
 
         use_thread(self.cameras_array, worker)
-        
